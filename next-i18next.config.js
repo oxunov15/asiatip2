971c96ae450f43fc3950ef83587df13077364a38
oxunov15 <oxunjonovabdusamig638@gmail.com>
2025-06-30 10:02:42 +0500

    Add files via upload

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'uz',
+    locales: ['uz', 'ru', 'en', 'tr', 'kk'],
+  },
+  localePath: './public/locales',
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+};
